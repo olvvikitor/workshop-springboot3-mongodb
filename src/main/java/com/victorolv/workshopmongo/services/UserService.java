5f217180b1d003b0a8a278409087ac16f414b9f7
olviktor <126020776+olviktor@users.noreply.github.com>
2023-10-25 11:37:24 -0300

    Insert User POST

--- src/main/java/com/victorolv/workshopmongo/services/UserService.java
+++ src/main/java/com/victorolv/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victorolv.workshopmongo.domain.User;
+import com.victorolv.workshopmongo.dto.UserDTO;
 import com.victorolv.workshopmongo.repository.UserRepository;
 import com.victorolv.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,13 @@ public class UserService  {
 	public User findById(String id) {
 		 Optional<User> obj  = repo.findById(id);
 		 return obj.orElseThrow(()->  new ObjectNotFoundException("Usuario nao existe"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+		
+	}
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(),objdto.getName(),objdto.getEmail());
 	}
 
 }
